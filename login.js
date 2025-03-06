e269e26e5250ce398c8b826954f82afe8f8a3057
cproa <stacknnes@gmail.com>
2025-03-05 21:47:30 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,47 +1,17 @@
-// Simulamos el archivo JSON que contiene los usuarios y contraseñas
-const usuarios = {
-  "49460426": {
-    id: 0,
-    contraseña: "2277",
-    nombreCompleto: "Andrés Gabriel Morillo Figueroa",
-    añoCursando: 4,
-    comportamiento: "exelente",
-    correo: "agmorillofigueroa@escuelasproa.edu.ar",
-    OP: true  // Variable OP para este usuario
-  },
-  "50245080": { 
-    id: 1,
-    contraseña: "1messigay5",
-    nombreCompleto: "Ariel Valentin Ruizdiaz",
-    añoCursando: 4,
-    comportamiento: "nefasto",
-    correo: "avruizdiaz@escuelasproa.edu.ar",
-    OP: false  // Variable OP para este usuario
-  },
-  "49757518": {
-    id: 2,
-    contraseña: "petit",
-    nombreCompleto: "Lautaro Otto Breininger",
-    añoCursando: 4,
-    comportamiento: "autista",
-    correo: "lbreininger@escuelasproa.edu.ar",
-    OP: true  // Variable OP para este usuario
-  },
-  "51420730": {
-    id: 3,
-    contraseña: "Safirceballos",
-    nombreCompleto: "Safir Tiziano Ceballos",
-    añoCursando: 2,
-    comportamiento: "Regular",
-    correo: "schavezceballos@escuelasproa.edu.ar",
-    OP: false  // Variable OP para este usuario
-  }
-};
+// Cargar los usuarios desde el archivo JSON usando fetch
+async function cargarUsuarios() {
+  const response = await fetch('personas.json');
+  const usuarios = await response.json();
+  return usuarios;
+}
 
 // Función para manejar el inicio de sesión
-function iniciarSesion() {
+async function iniciarSesion() {
   let nombre = document.getElementById('nombre').value;
   let contrasena = document.getElementById('contrasena').value;
+
+  // Cargar los usuarios desde el archivo JSON
+  const usuarios = await cargarUsuarios();
 
   // Verificar si el nombre de usuario (DNI) existe y si la contraseña es correcta
   if (usuarios[nombre] && usuarios[nombre].contraseña === contrasena) {
